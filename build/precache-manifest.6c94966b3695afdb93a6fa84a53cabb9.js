d53ca88d9f6ad4af9c35cb22e6295952e64830a9
Enes EFE <enes_efe_158@hotmail.com>
2021-08-13 19:42:15 +0300

    build page changed

---
+++ build/precache-manifest.6c94966b3695afdb93a6fa84a53cabb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d726a57384ff07128319c994a37e3047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d73aeee9c935589718",
+    "url": "/static/css/main.abfa7809.chunk.css"
+  },
+  {
+    "revision": "ed685f4d1ac71715e976",
+    "url": "/static/js/2.dd09a6bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dd09a6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d73aeee9c935589718",
+    "url": "/static/js/main.48c7b7ca.chunk.js"
+  },
+  {
+    "revision": "976e9d77ee5dd28c4220",
+    "url": "/static/js/runtime-main.147274ae.js"
+  },
+  {
+    "revision": "963bd212ed66b65cb48ebb99a22377df",
+    "url": "/static/media/Logo.963bd212.png"
+  },
+  {
+    "revision": "ca3e33ae50a0fd8d34102a61b24379e5",
+    "url": "/static/media/code.ca3e33ae.svg"
+  },
+  {
+    "revision": "d3ebbca7a87141a1be1a0ee93975998c",
+    "url": "/static/media/myPhoto.d3ebbca7.jpeg"
+  }
+]);
